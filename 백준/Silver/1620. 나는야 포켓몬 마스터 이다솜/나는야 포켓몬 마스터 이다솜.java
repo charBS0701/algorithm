db4e14bb7d90f9759a5b8915e033dfc7c568264f
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-01-21 15:25:07 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 552 ms, Memory: 62272 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,30 +1,40 @@
-import java.util.*;
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 포켓몬의 개수 // 1<= <=100,000
-		int M = sc.nextInt(); // 맞춰야 하는 문제 수 // 1<= <=100,000
-		
-		Map<Integer, String> dic1 = new HashMap<>();		// 포켓몬 도감
-		Map<String, Integer> dic2 = new HashMap<>();		// 포켓몬 도감
-		
-		for (int n = 1; n <= N; n++) {
-			String name = sc.next();
-			dic1.put(n,name);	// 도감 입력
-			dic2.put(name, n);	// 도감 입력
-		}
-		
-		for (int m = 0; m < M; m++) {
-			String q = sc.next();	// 문제 입력 // 알파벳 or 번호
-			
-			// 숫자를 입력받은경우		// O(n)
-			if (q.charAt(0) >= '0' && q.charAt(0) <= '9') {
-				Integer idx = Integer.valueOf(q);
-				System.out.println(dic1.get(idx));
-			} else {	// 알파뱃을 입력받은경우
-				System.out.println(dic2.get(q));
-			}		
-		}		
-    }	
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputNM = br.readLine().split(" ");
+        int N = Integer.parseInt(inputNM[0]);
+        int M = Integer.parseInt(inputNM[1]);
+
+        Map<String, Integer> nameToNumber = new HashMap<>();
+        Map<Integer, String> numberToName = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            String pokemon = br.readLine();
+            nameToNumber.put(pokemon, i);
+            numberToName.put(i, pokemon);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            String question = br.readLine();
+
+            if (Character.isDigit(question.charAt(0))) {
+                // If the first character is a digit, it's a number.
+                int number = Integer.parseInt(question);
+                sb.append(numberToName.get(number)).append("\n");
+            } else {
+                // Otherwise, it's a name.
+                sb.append(nameToNumber.get(question)).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
