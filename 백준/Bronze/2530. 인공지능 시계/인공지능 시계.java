93d49739f4998b2b31d06462d2301bd19afe851e
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-01-22 23:27:12 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int s = sc.nextInt();
+		int D = sc.nextInt();
+		
+		s+=D;
+		
+		while (s>=60) {
+				m++;
+				s-=60;
+		}
+		
+		while (m>=60) {
+				h++;
+				m-=60;
+		}
+		
+		while (h>=24) {
+			h-=24;
+		}
+		
+		System.out.println(h + " " + m + " " + s);
+	}	
+}
