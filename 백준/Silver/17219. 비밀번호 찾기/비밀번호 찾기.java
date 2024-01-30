91a8f7a6f618a46379249219c435e3b4e7f6746d
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-01-31 00:25:55 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 500 ms, Memory: 63732 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Map<String, String> map = new HashMap<>();
+		
+		for (int n = 0; n < N; n++) {					// 이메일, 비번 입력
+			st = new StringTokenizer(br.readLine());
+			map.put(st.nextToken(), st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int m = 0; m < M; m++) {					// 비번 찾기
+			String s = br.readLine();
+			sb.append(map.get(s)).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
