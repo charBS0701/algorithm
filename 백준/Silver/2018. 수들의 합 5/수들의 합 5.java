fde853169405d0ddf4c01121e30b0292b252b344
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-04 18:30:26 +0900

    [Silver V] Title: 수들의 합 5, Time: 168 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
-
+// two pointers
 public class Main {
 	static Scanner sc = new Scanner(System.in);
 	static int N;
 	static int count = 1;
+	static int start_index =1;
+	static int end_index=1;
+	static int sum = 1;
 
 	public static void main(String[] args) {
 		N = sc.nextInt();
 
-		for (int i = 1; i < N; i++) {
-			int sum = i;
-			for (int j = i + 1; j < N; j++) {
-				sum += j;
-				if (sum == N) {
-					count++;
-					break;
-				} else if (sum > N)
-					break;
+		while (end_index != N) {
+			if (sum == N) {	// 현재 연속 합이 N과 같은 경우
+				count++;
+				end_index++;
+				sum = sum + end_index;
+			} else if (sum>N) {	// 현재 연속 합이 N보다 더 큰 경우
+				sum = sum - start_index;
+				start_index++;
+			} else {	// 현재 연속 합이 N보다 작은 경우
+				end_index++;
+				sum = sum + end_index;
 			}
 		}
-
+		
 		System.out.println(count);
 	}
-
 }
