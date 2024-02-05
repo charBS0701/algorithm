b5a370a5124157a35741e2e08520c5f38da7a179
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-05 12:18:07 +0900

    Update 탑.java
    주석추가

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -24,21 +24,19 @@ public class Main {
 		Deque<Integer> stack = new ArrayDeque<>();
 		stack.push(1);
 		for (int i = 2; i <= N; i++) {
-			while (!(stack.isEmpty())) {	// 스택에 원소가 있으면
-				if (arr[i] >= arr[stack.peek()]) {	// 
-					stack.pop();
-				} else {
-					sb.append(stack.peek()).append(" ");
-					stack.push(i);
+			while (!(stack.isEmpty())) {	// 스택에 원소가 있으면 반복
+				if (arr[i] >= arr[stack.peek()]) {	// 스택의 top 보다 새로운 탑이 높으면
+					stack.pop();					// pop
+				} else {	
+					sb.append(stack.peek()).append(" ");	// 스택의 탑이 높으면 그 탑이 답
+					stack.push(i);							// 새로운 탑 push
 					break;
 				}
 			}			
-			if (stack.isEmpty()) sb.append(0).append(" ");
+			if (stack.isEmpty()) sb.append(0).append(" ");	// 새로운 탑이 가장 높으면 이전탑 다 지워졌음
 			
-			stack.push(i);
+			stack.push(i);	// 새로운 탑 push
 		}
-		
-		
 		
 		System.out.println(sb);
 	}
