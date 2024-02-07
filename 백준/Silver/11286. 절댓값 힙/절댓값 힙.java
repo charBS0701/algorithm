eafb1422699d92194751bfd079d94dff1e47da75
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-07 15:55:24 +0900

    [Silver I] Title: 절댓값 힙, Time: 424 ms, Memory: 28776 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,34 +3,31 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine()); // 연산의 개수 N(1≤N≤100,000)
-		
-		PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-			int abs1 = Math.abs(o1);
-			int abs2 = Math.abs(o2);
-			
-			return abs1!=abs2 ? abs1-abs2 : (o1 > o2 ? 1 : -1);
-			
-		});
-		
-		
-		// 수행
-		for (int n = 0; n < N; n++) {
-			int command = Integer.parseInt(br.readLine());
 
-			if (command == 0) {		// 제거
-				if (queue.isEmpty()) sb.append(0 + "\n");
-				else sb.append(queue.poll() + "\n");
-			} else {				// 추가
-				queue.add(command);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static PriorityQueue<Integer> pq = new PriorityQueue<>( // 절댓값이 작은순서, 같으면 음수우선
+			(o1, o2) -> {
+				return Math.abs(o1) == Math.abs(o2) ? o1 - o2 : Math.abs(o1) - Math.abs(o2);
+			});
+
+	public static void main(String[] args) throws Exception {
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if (input == 0) {
+				if (pq.isEmpty())
+					sb.append(0).append("\n");
+				else
+					sb.append(pq.poll()).append("\n");
+				
+			} else {
+				pq.offer(input);
 			}
 		}
-		
-		System.out.println(sb);
 
+		System.out.println(sb);
 	}
+
 }
