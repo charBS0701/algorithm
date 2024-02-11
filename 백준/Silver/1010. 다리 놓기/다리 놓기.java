f4a040ab0ca0aacec3b36c2080d8ca666a2ec73b
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-11 16:49:10 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 11836 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -4,39 +4,37 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	// 조합을 계산하는 함수
-	public static long combination(int n, int r) {
-		// 0 또는 1인 경우는 바로 반환
-		if (r == 0 || n == r) {
-			return 1;
-		}
-
-		// 파스칼의 삼각형을 이용하여 조합 계산
-		long[][] dp = new long[n + 1][r + 1];
-		for (int i = 0; i <= n; i++) {
-			dp[i][0] = 1;
-		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= i && j <= r; j++) {
-				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-			}
-		}
-
-		return dp[n][r];
-	}
+	static int[][] dp;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		dp = new int[30][30];
+
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 30; j++) {
+				if (i == j || j == 0)
+					dp[i][j] = 1;
+			}
+		}
+		
+		for (int i = 2; i < 30; i++) {
+			for (int j = 1; j < i; j++) {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+			}
+		}
+		
+		
+
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // N, M (0 < N ≤ M < 30)
+			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			long result = combination(M, N); // 수정된 조합 함수 호출
-			sb.append(result).append("\n");
+
+			sb.append(dp[M][N]).append("\n");
 		}
 		System.out.println(sb);
 	}
+
 }
