1241b59b5eb61b6af5d81f47e6281dbb8bfff643
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-20 12:46:49 +0900

    [Gold III] Title: 줄 세우기, Time: 416 ms, Memory: 45092 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -8,58 +8,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static List<Integer>[] graph;
-	static int indegrees[];
+	static int[] topo; // 진입차수 관리
+	static List<List<Integer>> graph = new ArrayList<>(); // 순서 - 연결
+	static Queue<Integer> que = new ArrayDeque<>();
 	static StringBuilder sb = new StringBuilder();
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		graph = new ArrayList[N+1];
-		indegrees = new int[N+1];	// 진입차수 수
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<Integer>();
-		}
-		for (int i = 0; i < M; i++) {					// 선후관계 입력
-			st = new StringTokenizer(br.readLine());
-			int small = Integer.parseInt(st.nextToken());
-			int tall = Integer.parseInt(st.nextToken());
-			graph[small].add(tall);
-			indegrees[tall]++;	// 진입차수 수 증가
+
+		topo = new int[N + 1]; // 0 dummy
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
 		}
 
-		bfs();
+		for (int i = 0; i < M; i++) { // 선후관계 입력
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			graph.get(A).add(B);
+			topo[B]++; // 진입 차수 증가
+		}
+
+		// bfs <= 진입차수가 0인 정점(학생)을 모두 queue 에 넣고 시작
+		// 출력은 꺼내면서 만들면 된다.
+		// queue 에서 꺼내서 이동할 수 있는 (학생 순서를 세우는 - 꺼낸 학생 뒤에 오는 학생) 정점으로 이동 
+		// 그 정점의 진입차수를 1개 줄인다. 만약 줄이고 난 뒤 진입차수가 0이면 queue 에 넣는다.
 		
-		System.out.println(sb);
-	}
-	
-	static void bfs() {
-		Queue<Integer> que = new ArrayDeque<>();
-		// 진입차수가 0인 것들을 인큐
 		for (int i = 1; i <= N; i++) {
-			if (indegrees[i] == 0) {
+			if(topo[i] == 0) {
 				que.offer(i);
-				indegrees[i]-=1;
-				sb.append(i).append(" ");
 			}
 		}
 		
-		while(!que.isEmpty()) {
-			// 디큐 
-			int now = que.poll();
-			for (int i : graph[now]) {
-					indegrees[i]--;
-					if (indegrees[i] == 0) {
-						que.offer(i);
-						indegrees[i]--;	// 방문처리
-						sb.append(i).append(" ");	// 출력
-						
-				
+		while (! que.isEmpty() ) {
+			int no = que.poll();
+			// 출력
+			sb.append(no).append(" ");
+			
+			// 꺼낸 번호(학생)에서 갈 수 있는 번호 탐색
+			List<Integer> list = graph.get(no);
+			int size = list.size();
+			for (int i = 0; i < size; i++) {
+				int temp = list.get(i);
+				// 차수감소
+				topo[temp]--;
+				if (topo[temp] == 0) {
+					que.offer(temp);
 				}
 			}
-			
 		}
+		System.out.println(sb);
+		
 	}
 }
