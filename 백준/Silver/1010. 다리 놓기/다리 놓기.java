678e82614e68a4a0b87bd804275880d2b4649e92
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-02-28 11:34:34 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -3,38 +3,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static int T;
+	static int N,M;
 	static int[][] dp;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		T = Integer.parseInt(br.readLine());
+		
 		dp = new int[30][30];
-
+		
 		for (int i = 0; i < 30; i++) {
-			for (int j = 0; j < 30; j++) {
-				if (i == j || j == 0)
-					dp[i][j] = 1;
+			for (int j = 0; j <= i; j++) {
+				if (j==0 || i==j) dp[i][j] = 1;
+				else dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
 			}
 		}
 		
-		for (int i = 2; i < 30; i++) {
-			for (int j = 1; j < i; j++) {
-				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-			}
-		}
-		
-		
-
-		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 			sb.append(dp[M][N]).append("\n");
 		}
 		System.out.println(sb);
 	}
-
 }
