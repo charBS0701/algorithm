08e0e77cd7675c7a9663e24abb5ef2aed4956c22
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-03-29 16:35:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 760 ms, Memory: 58196 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int V, E, result;
+	static boolean[] visit;
+	static List<List<int[]>> list = new ArrayList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		visit = new boolean[V + 1];
+		for (int i = 0; i <= V; i++) {
+			list.add(new ArrayList<>());
+		}
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			list.get(s).add(new int[] { e, w });
+            list.get(e).add(new int[] { s, w });
+		}
+
+		// 풀이
+		prim(1);
+		System.out.println(result);
+
+	}
+
+	static void prim(int start) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]); // 가중치작은순
+		pq.addAll(list.get(start));
+		visit[start] = true;
+
+		while (!pq.isEmpty()) {
+			int[] now = pq.poll();
+			if (visit[now[0]])
+				continue;
+			visit[now[0]] = true;
+			result += now[1];
+			for (int[] next : list.get(now[0])) {
+				if (!visit[next[0]])
+					pq.add(next);
+			}
+
+		}
+
+	}
+}
