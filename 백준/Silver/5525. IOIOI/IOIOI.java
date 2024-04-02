bba4d57d76c8a6a2709a08df32339b98349990ad
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-04-02 09:48:58 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 20396 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,27 +4,57 @@ import java.io.InputStreamReader;
 public class Main {
 	
 	static int N, M, result;
+	static int I, O, iCnt, oCnt;
 	static String S;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());	// 1 ≤ N ≤ 1,000,000
 		M = Integer.parseInt(br.readLine());	// 2N+1 ≤ M ≤ 1,000,000
 		S = br.readLine();
 		
-		for (int i = 1; i <= N; i++) {
-			if (i==1) sb.append("I").append("O").append("I");
-			else sb.append("O").append("I");
+		I = N+1;
+		O = N;
+		
+		for (int i = 0; i < M; i++) {
+			char c = S.charAt(i);
+			
+			if (c == 'I') {
+				if (i==0) iCnt++;
+				else {
+					char prev = S.charAt(i-1);
+					if (prev == 'I') {
+						init();
+						iCnt++;
+					} else if (prev == 'O') {
+						iCnt++;
+					}
+				}
+			} else if (c == 'O') {
+				if (i==0) init();
+				else {
+					char prev = S.charAt(i-1);
+					if (prev == 'I') {
+						oCnt++;
+					} else if( prev == 'O') {
+						init();
+					}
+				}
+			}
+			
+			if (iCnt == I && oCnt == O) {	// 찾음
+				result++;
+				iCnt--;
+				oCnt--;
+			}
 		}
 		
-		// 풀이
-		int subSize = sb.length();
-		
-		for (int i = 0; i <= M-subSize; i++) {
-			if (S.substring(i,i+subSize).equals(sb.toString())) result++;
-		}
-		
+		// 출력
 		System.out.println(result);
+	}
+	
+	static void init() {
+		iCnt = 0;
+		oCnt = 0;
 	}
 	
 }
