84df62aff29d4e70e069d82632b4ad72fd65389c
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-04-15 21:59:26 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -3,29 +3,29 @@
 
 import java.io.*;
 import java.util.*;
-import java.math.BigInteger;
 
 public class Main {
+    static int N;
+    static int two, five;
 
-	static int N;
-	static BigInteger res = new BigInteger("1");
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			res = res.multiply(new BigInteger(Integer.toString(i)));
-		}
-		
-		String str = res.toString();
-		
-		int count = 0;
-		for (int i = str.length()-1; i >= 0; i--) {
-			if (str.charAt(i) == '0') count++;
-			else {
-				System.out.println(count);
-				return;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for (int i = 2; i <= N; i++) {
+        	int n = i;
+			while (n%2==0 || n%5==0) {
+				if (n%2==0) {
+					n/=2;
+					two++;
+				}
+				if (n%5==0) {
+					n/=5;
+					five++;
+				}
 			}
 		}
-	}
+        
+        System.out.println(Math.min(two, five));
+        
+    }
 }
