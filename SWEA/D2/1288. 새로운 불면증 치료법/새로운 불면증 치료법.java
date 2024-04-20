80b0d86cbc9dcb7afda0028f9c263740cc5e4c58
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-04-20 10:47:19 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 149 ms, Memory: 21,376 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,42 +1,33 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int N = sc.nextInt(); // 1 ~ 1,000,000
-			int result = 0;
-			int[] flags = new int[10];
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int T, N, result;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
+			int goal = (1<<10)-1;
+			int now = 0;
 			
-			for (int i=1; ; i++) {
-				int num = N*i;
-				
-				// 각 자리수 체크
-				while (num>0) {
-					flags[num%10]++;
-					num/=10;
-				}
-				
-				// flags 체크
-				int totalFlag = 1;
-				for (int j=0; j<10; j++) {
-					if (flags[j]==0) {
-						totalFlag=0;
-						break;
-					}
-				}
-				
-				if (totalFlag == 1) {
-					result = i*N;
-					break;
+			for (result = 1; goal != now; result++) {
+				int num = N*result;
+				while (num != 0) {
+					int tmp = num % 10;
+					now = (1<<tmp) | now;
+					num /= 10;
 				}
 			}
-			System.out.printf("#%d %d\n", test_case, result);
+			
+			
+			sb.append("#").append(t).append(" ").append((result-1)*N).append("\n");
 		}
+		
+		System.out.println(sb);
+
 	}
-}
+
+}	
