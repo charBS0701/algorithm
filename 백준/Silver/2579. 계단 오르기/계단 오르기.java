b9708eaff6a879b0d7d0a5be7ea37f026ed3c6e2
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-04-20 23:00:35 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,38 +1,44 @@
+// 2579 계단 오르기
+// https://www.acmicpc.net/problem/2579
+
+import java.io.*;
 import java.util.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int N;
+	static int[] stairs;
+	static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		stairs = new int[N+1];
+		dp = new int[N+1];
 		
-		int N = sc.nextInt(); 	// 계단의 수
-		int[] stairs = new int[N+1];
-		
-		for (int n=1; n<=N; n++) {	// 계단당 점수 입력
-			stairs[n] = sc.nextInt();
+		for (int n = 1; n <= N; n++) {
+			stairs[n] = Integer.parseInt(br.readLine());
 		}
 		
-		if (N==1) {
-			System.out.println(stairs[1]);
-			return;
-		} else if (N==2) {
-			System.out.println(stairs[1]+stairs[2]);
-			return;
-		} else if (N==3) {
-			System.out.println(Math.max(stairs[1]+stairs[3], stairs[2]+stairs[3]));
-			return;
-		}
-		
-		int[] dp = new int[N+1];	// 각 계단에 올랐을 때 얻을 수 있는 최대 점수
 		dp[1] = stairs[1];
-		dp[2] = dp[1]+stairs[2];
-		// dp3 은 1번째+3번째, 2번째+3번째 중 큰 것
-		dp[3] = Math.max( dp[1]+stairs[3] , stairs[2]+stairs[3] );
-		
-		for (int n=4; n<=N; n++) {
-			// dp[n] = (2번째 전dp + 마지막 칸), (3번째 전dp + 1번째 전 + 마지막 칸) 중 큰 것  
-			dp[n] = Math.max(dp[n-2]+stairs[n], dp[n-3]+stairs[n-1] + stairs[n]);
+		if(N == 1) {
+			System.out.println(dp[1]);
+			return;
 		}
+		dp[2] = dp[1] + stairs[2];
+		if(N == 2) {
+			System.out.println(dp[2]);
+			return;
+		}
+		
+		dp[3] = Math.max(dp[1] + stairs[3], dp[0] + stairs[2] + stairs[3]); 
+		
+		for (int i = 4; i <= N; i++) {
+			dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i]);
+		}
+		
 		
 		System.out.println(dp[N]);
+		
 	}
 }
