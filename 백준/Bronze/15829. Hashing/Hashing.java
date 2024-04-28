bf21c0703dd1151926990b43aa17a91efe82ea43
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-04-29 00:16:46 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14140 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int L;
+	static char[] arr;
+	static long result;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		L = Integer.parseInt(br.readLine());
+		arr = br.readLine().toCharArray();
+
+		result = (int) arr[0] - 'a' + 1;	// 맨 앞 자리
+		
+		for (int i = 1; i < L; i++) {
+			int a = (int) arr[i] - 'a' + 1;
+			
+			int tmp = 1;
+			for (int j = 1; j <= i; j++) {
+				tmp = tmp * 31 % 1234567891;	// 모듈러 연산
+			}
+			result += a * tmp;
+		}
+
+		System.out.println(result);
+
+	}
+}
