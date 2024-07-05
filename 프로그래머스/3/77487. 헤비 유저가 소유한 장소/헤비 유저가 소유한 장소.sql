8801589dd8f1ac9e651aa6f18eb704774dd97c18
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-07-05 23:21:07 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+WITH HEAVY_USER AS(
+    SELECT host_id
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2)
+
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT host_id
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2)
+ORDER BY ID;
