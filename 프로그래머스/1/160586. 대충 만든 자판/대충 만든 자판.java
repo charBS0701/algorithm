ebb6ee26a3f5500338141e7e270c34d4d37cd03a
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-07-07 17:24:00 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.56 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,37 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];
-        
-        Map<Character,Integer> map = new HashMap<>();
-        for (String str : keymap) {
-            for (int i=0; i<str.length(); i++) {
-                char c = str.charAt(i);
-                if (!map.containsKey(c)) {
-                    map.put(c,i+1);
-                } else {
-                    if (map.get(c) > i+1) map.put(c,i+1);
-                }
-            }
-        }
-        
-        for (int i=0; i<targets.length; i++) {
-            int sum = 0;
-            boolean flag = true;
-            String target = targets[i];
-            for (int j=0; j<target.length(); j++) {
-                char c = target.charAt(j);
-                if (!map.containsKey(c)) {
-                    answer[i] = -1;
-                    flag = false;
-                    break;
-                } else {
-                    sum += map.get(c);
-                }
-            }
-            if (flag) answer[i] = sum;
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
