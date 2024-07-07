cf1a3cd998c15e5efe2bcb5acefd90753cd1b2c2
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-07-07 16:56:14 +0900

    [level 1] Title: 소수 만들기, Time: 5.09 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class Solution {
+    static int answer;
+    static boolean[] isPrime = new boolean[50_000];
     public int solution(int[] nums) {
-        int answer = 0;
-        boolean[] isPrime = new boolean[50_000];
         Arrays.fill(isPrime, true);
         // get Prime numbers
         for (int i=2; i*i<50_000; i++) {
@@ -12,20 +12,42 @@ class Solution {
             }
         }
         
-        // n C 3
-        for (int i=0; i<nums.length-2; i++) {
-            int sum = nums[i];
-            for (int j=i+1; j<nums.length-1; j++) {
-                sum += nums[j];
-                for (int k=j+1; k<nums.length; k++) {
-                    sum += nums[k];
-                    if (isPrime[sum]) answer++;
-                    sum -= nums[k];
-                }
-                sum -= nums[j];
-            }
-        }
+        // // n C 3
+        // for (int i=0; i<nums.length-2; i++) {
+        //     int sum = nums[i];
+        //     for (int j=i+1; j<nums.length-1; j++) {
+        //         sum += nums[j];
+        //         for (int k=j+1; k<nums.length; k++) {
+        //             sum += nums[k];
+        //             if (isPrime[sum]) answer++;
+        //             sum -= nums[k];
+        //         }
+        //         sum -= nums[j];
+        //     }
+        // }
+        
+        src = nums;
+        selected = new boolean[src.length];
+        comb(0,0);
 
         return answer;
+    }
+    
+    static int[] tgt = new int[3];
+    static int[] src;
+    static boolean[] selected;
+
+    
+    static void comb(int srcIdx, int tgtIdx) {
+        if (tgtIdx == 3) {
+            if (isPrime[Arrays.stream(tgt).sum()]) answer++;
+            return;
+        }
+        
+        for (int i=srcIdx; i<src.length; i++) {
+            tgt[tgtIdx] = src[i];
+            comb(i+1, tgtIdx+1);
+        }
+        
     }
 }
