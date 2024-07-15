0ca663a3df417f1c981ec668683a931d2c32bbb8
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-07-15 20:36:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int y;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        y = Integer.parseInt(br.readLine());
+        
+        System.out.println(y - (2541-1998));
+
+    }
+}
