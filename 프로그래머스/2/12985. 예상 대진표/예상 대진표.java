40062bdfd8ba4f4d3d2c82e3f6e2cf3a9f83253f
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-08-17 14:58:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.09 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,27 @@
+class Solution
+{
+    static int answer = 0;
+    public int solution(int n, int a, int b)
+    {
+        a = ((a-1) / 2) + 1;
+        b = ((b-1) / 2) + 1;
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        
+        int jisu = 0;
+        while (n != 1) {
+            n = n >> 1;
+            jisu++;
+        }
+        
+        answer++;   // 1차에서 같은 조 일 때
+        while (true) {
+            if (a == b) break;
+            answer++;
+            a = (a-1) / 2 + 1;
+            b = (b-1) / 2 + 1;
+        }
+        
+        return answer;
+    }
+}
