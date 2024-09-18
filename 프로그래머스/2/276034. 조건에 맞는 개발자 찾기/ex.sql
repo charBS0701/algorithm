a2e620c0af155c35942d2f930954160d16e77dca
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-09-18 21:37:14 +0900

    Create ex.sql

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/ex.sql
@@ -0,0 +1,10 @@
+SELECT
+    id, email, first_name, last_name
+FROM developers
+WHERE
+    skill_code & (
+        SELECT SUM(code)
+        FROM SKILLCODES
+        WHERE NAME IN ("Python", "C#")
+    ) != 0
+ORDER BY ID
