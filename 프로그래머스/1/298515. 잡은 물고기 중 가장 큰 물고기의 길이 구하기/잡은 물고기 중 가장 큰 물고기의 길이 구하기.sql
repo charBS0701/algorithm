6740c38065d5632c31e2afbff709a55e7456243d
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2024-09-20 23:27:54 +0900

    [level 1] Title: 잡은 물고기 중 가장 큰 물고기의 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298515. 잡은 물고기 중 가장 큰 물고기의 길이 구하기/잡은 물고기 중 가장 큰 물고기의 길이 구하기.sql
+++ 프로그래머스/1/298515. 잡은 물고기 중 가장 큰 물고기의 길이 구하기/잡은 물고기 중 가장 큰 물고기의 길이 구하기.sql
@@ -1,10 +1,5 @@
 -- 코드를 작성해주세요
 SELECT 
-    CONCAT(LENGTH,'cm') MAX_LENGTH
+    CONCAT(MAX(LENGTH),'cm') MAX_LENGTH
 FROM
-    FISH_INFO
-WHERE
-    LENGTH = (SELECT MAX(LENGTH)
-                FROM FISH_INFO)
-        ;
-    
+    FISH_INFO;
