9146496a866476a95d3898ce363eded44740ef7f
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2025-02-05 03:52:17 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 604 ms, Memory: 50180 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,77 +1,82 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-// 크루스칼
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	static int V, E, result;
-	static List<Edge> list = new ArrayList<>();
-	static int[] parent;
-	
-	static class Edge{
-		int v1, v2, w;
-		Edge(int v1, int v2, int w) {
-			this.v1=v1; this.v2=v2; this.w=w;
+	static class Edge implements Comparable<Edge> {
+    	int from, to, weight;
+        
+        public Edge(int from, int to, int weight) {
+        	super();
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo(Edge o) {
+        	return Integer.compare(this.weight, o.weight);
 		}
 	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			list.add(new Edge(s,e,w));
+    
+    static int V;	// 정점 개수
+    static Edge[] edgeList;
+    static int[] parents;
+    
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());		// 정점의 개수 입력
+        int E = Integer.parseInt(st.nextToken());	// 간선의 개수 입력
+        edgeList = new Edge[E];
+        
+        // from to weigth 로 간선 정보의 입력이 들어온다고 가정
+        for (int i=0; i<E; i++) {
+        	st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            edgeList[i] = new Edge(from, to, weight);
+        } // 간선 리스트 생성
+        
+        // 전처리 : 간선 리스트 오름차순 정렬
+        Arrays.sort(edgeList);
+        
+        // 1. make - set
+        make();
+        
+        // 2. 정렬된 간선하나씩 꺼내어 신장트리 생성
+        int weight = 0;
+        int cnt = 0;
+        for (Edge edge : edgeList) {
+        	if (!union(edge.from, edge.to)) continue;
+            weight += edge.weight;
+            if (++cnt == V-1) break;		// 최소신장트리(MST) 완성
 		}
-		
-		Collections.sort(list, (e1, e2) -> e1.w - e2.w);  // 정렬
-
-		// 풀이
-		makeSet();
-		kruskal();
-		System.out.println(result);
-
+        
+        System.out.println(weight);
+    	
 	}
-
-	static void kruskal() {
-		int count = 0;
-		for(Edge edge : list) {
-			if (count == V-1) break;
-			if (!union(edge.v1, edge.v2)) continue;
-			result += edge.w;
-			count++;
-		}
+    
+     public static void make() {
+     	parents = new int[V+1]; 	// 자신의 부모나 루트(경로 압축) 저장
+     	for (int i=1; i<=V; i++) {
+        	parents[i] = i;		// 모든 정점을 자신을 루트로
+        }
 	}
-	
-	static void makeSet() {
-		parent = new int[V+1];
-		for (int i = 1; i <= V; i++) {
-			parent[i] = i;
-		}
+    
+    public static int find(int a) {		// a가 속한 트리의 루트 찾기
+		if (a == parents[a]) return a;
+        
+        return parents[a] = find(parents[a]);		// 경로 압축
 	}
-	static int findSet(int x) {
-		if (parent[x] == x) return x;
-		else return parent[x] = findSet(parent[x]);
-	}
-	
-	static boolean union(int x1, int x2) {
-		int p1 = findSet(x1);
-		int p2 = findSet(x2);
-		if (p1 == p2) return false;
-		else {
-			if (p1 < p2) {
-				parent[p2] = parent[p1];
-			} else {
-				parent[p1] = parent[p2];
-			}
-			return true;
-		}
+    
+    public static boolean union(int a, int b) {
+    	int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false;		// a,b 가 같은 트리에 속해있다 => union 불필요
+        
+        parents[bRoot] = aRoot;
+        return true;
 	}
 }
