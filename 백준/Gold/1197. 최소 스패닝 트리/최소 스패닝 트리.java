2d9153cbdceff9f2a4adf960cb213bfd2172ee9d
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2025-02-06 21:28:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 54756 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,82 +1,91 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+// 프림
+// cycle check <- visit
 public class Main {
-	static class Edge implements Comparable<Edge> {
-    	int from, to, weight;
-        
-        public Edge(int from, int to, int weight) {
-        	super();
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
+
+	static int V, E;
+	static long sum; // V - 1 개의 간선의 가중치의 합
+
+	static List<List<Edge>> adjList; // 인접 리스트
+	static boolean[] visit; // 방문(선택) 체크
+
+	static PriorityQueue<Edge> pqueue = new PriorityQueue<>((e1, e2) -> e1.c - e2.c);
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList<>();
+        for (int i = 0; i <= V; i++) { // 0 dummy
+			adjList.add(new ArrayList<>());
         }
-        
-        @Override
-        public int compareTo(Edge o) {
-        	return Integer.compare(this.weight, o.weight);
+        visit = new boolean[V + 1];
+
+        for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            adjList.get(v1).add(new Edge(v2, c));
+            adjList.get(v2).add(new Edge(v1, c));
+        }
+
+		// 풀이
+        sum = 0;
+        pqueue.clear();
+		prim();
+
+		System.out.println(sum);
+	}
+	
+	static void prim() {
+		// 시작 정점 1번 출발 (다른 번호도 상관없음)
+		int cnt = 1;
+		visit[1] = true;
+		
+		pqueue.addAll(adjList.get(1));	// 1번 정점에서 갈 수 있는 모든 정점, 간선을 담는다.
+		
+		while( !pqueue.isEmpty() ) {
+			Edge edge = pqueue.poll();
+			if( visit[edge.v] ) continue;
+			
+			visit[edge.v]= true;
+			sum += edge.c;	// 최소 비용 합 계산
+			
+			cnt++;
+			if (cnt==V) break; 	// 모든 정점을 선택
+			
+//			pqueue.addAll(adjList.get(edge.v)); // 이미 방문한 정점이 포함된다.
+			
+			List<Edge> temp = adjList.get(edge.v);
+			int size = temp.size();
+			for (int i = 0; i < size; i++) {
+				Edge e = temp.get(i);
+				if (!visit[e.v]) pqueue.add(e); 
+			}
+			
 		}
 	}
-    
-    static int V;	// 정점 개수
-    static Edge[] edgeList;
-    static int[] parents;
-    
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        V = Integer.parseInt(st.nextToken());		// 정점의 개수 입력
-        int E = Integer.parseInt(st.nextToken());	// 간선의 개수 입력
-        edgeList = new Edge[E];
-        
-        // from to weigth 로 간선 정보의 입력이 들어온다고 가정
-        for (int i=0; i<E; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            
-            edgeList[i] = new Edge(from, to, weight);
-        } // 간선 리스트 생성
-        
-        // 전처리 : 간선 리스트 오름차순 정렬
-        Arrays.sort(edgeList);
-        
-        // 1. make - set
-        make();
-        
-        // 2. 정렬된 간선하나씩 꺼내어 신장트리 생성
-        int weight = 0;
-        int cnt = 0;
-        for (Edge edge : edgeList) {
-        	if (!union(edge.from, edge.to)) continue;
-            weight += edge.weight;
-            if (++cnt == V-1) break;		// 최소신장트리(MST) 완성
+
+	// 특정 정점에서부터 갈 수 있는 간선
+	// 시작정점 X <= 다른 자료구조에 시작정점을 관리 3 -> 2, 3 -> 4
+	static class Edge {
+		int v, c;
+
+		Edge(int v, int c) {
+			this.v = v;
+			this.c = c;
 		}
-        
-        System.out.println(weight);
-    	
-	}
-    
-     public static void make() {
-     	parents = new int[V+1]; 	// 자신의 부모나 루트(경로 압축) 저장
-     	for (int i=1; i<=V; i++) {
-        	parents[i] = i;		// 모든 정점을 자신을 루트로
-        }
-	}
-    
-    public static int find(int a) {		// a가 속한 트리의 루트 찾기
-		if (a == parents[a]) return a;
-        
-        return parents[a] = find(parents[a]);		// 경로 압축
-	}
-    
-    public static boolean union(int a, int b) {
-    	int aRoot = find(a);
-        int bRoot = find(b);
-        if (aRoot == bRoot) return false;		// a,b 가 같은 트리에 속해있다 => union 불필요
-        
-        parents[bRoot] = aRoot;
-        return true;
 	}
 }
