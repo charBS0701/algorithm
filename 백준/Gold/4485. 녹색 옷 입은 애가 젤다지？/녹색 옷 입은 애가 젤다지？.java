450e4a3cff2595907283917086fc354cf3e51210
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2025-02-09 18:54:12 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 540 ms, Memory: 57224 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,90 +1,74 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    
-    static class Vertex {
-        int r,c;
-        int w;
-        
-        public Vertex(int r, int c, int w) {
-            this.r=r;
-            this.c=c;
-            this.w=w;
-        }
+class Node implements Comparable<Node> {
+    int x, y, cost;
+
+    Node(int x, int y, int cost) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
     }
-    
-    static int N;
-    static List<Vertex>[][] mat;
-    static int[][] originMat;
-    static int[][] cost;
-    static int[] dy = {-1,1,0,0};
-    static int[] dx = {0,0,-1,1};
+
+    @Override
+    public int compareTo(Node other) {
+        return this.cost - other.cost;
+    }
+}
+
+public class Main {
     static final int INF = Integer.MAX_VALUE;
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         
-        int t = 1;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int problemCount = 1;
+        
         while (true) {
-            N = Integer.parseInt(br.readLine());
+            int N = sc.nextInt();
             if (N == 0) break;
+
+            int[][] cave = new int[N][N];
+            int[][] dist = new int[N][N];
             
-            originMat = new int[N][N];
-            mat = new ArrayList[N][N];
-            for (int i=0; i<N; i++) {
-                for (int j=0; j<N; j++) {
-                    mat[i][j] = new ArrayList<Vertex>();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    cave[i][j] = sc.nextInt();
+                    dist[i][j] = INF;
                 }
             }
-             
-            cost = new int[N][N];   
-            for (int i=0; i<N; i++) {
-                Arrays.fill(cost[i], INF);
-            }
             
-            StringTokenizer st = null;
-            for (int i=0; i<N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j=0; j<N; j++) {
-                    int weight = Integer.parseInt(st.nextToken());
-                    originMat[i][j] = weight;
-                    for (int d=0; d<4; d++) {
-                        int ny = i+dy[d];
-                        int nx = j+dx[d];
-                        if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
-                        mat[ny][nx].add(new Vertex(i,j,weight));
+            System.out.println("Problem " + problemCount + ": " + dijkstra(N, cave, dist));
+            problemCount++;
+        }
+    }
+
+    static int dijkstra(int N, int[][] cave, int[][] dist) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(0, 0, cave[0][0]));
+        dist[0][0] = cave[0][0];
+        
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int x = current.x;
+            int y = current.y;
+            int cost = current.cost;
+            
+            if (cost > dist[x][y]) continue;
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    int newCost = dist[x][y] + cave[nx][ny];
+                    if (newCost < dist[nx][ny]) {
+                        dist[nx][ny] = newCost;
+                        pq.offer(new Node(nx, ny, newCost));
                     }
                 }
             }
-            
-            dijkstra();
-            
-            sb.append("Problem ").append(t++).append(": ").append(cost[N-1][N-1]).append("\n");
-            
         }
-        
-        System.out.println(sb);
-    }
-    
-    static void dijkstra() {
-        PriorityQueue<Vertex> pq = new PriorityQueue<>((o1,o2) -> o1.w-o2.w);
-        pq.add(new Vertex(0,0,originMat[0][0]));
-        cost[0][0] = originMat[0][0];
-        
-        while (!pq.isEmpty()) {
-            Vertex now = pq.poll();
-            if (now.w > cost[now.r][now.c]) continue;
-            
-            for (Vertex nv : mat[now.r][now.c]) {
-                if (cost[nv.r][nv.c] > cost[now.r][now.c] + nv.w) {
-                    cost[nv.r][nv.c] = cost[now.r][now.c] + nv.w;
-                    pq.add(new Vertex(nv.r,nv.c,nv.w));
-                }
-            }
-        }
-        
-        
+        return dist[N - 1][N - 1];
     }
 }
