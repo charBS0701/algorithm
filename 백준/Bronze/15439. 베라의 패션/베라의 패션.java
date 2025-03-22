a11c31b73cdf37b1013d7d07c5ec850ad0089118
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2025-03-22 22:34:56 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static int N;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        System.out.println(N * (N-1));
+    }
+}
