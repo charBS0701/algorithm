3827bcd07d4b90009b59def97ce424110739c1b0
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2025-03-29 17:44:45 +0900

    [Bronze V] Title: Zadanie próbne, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        
+        sb = sb.reverse();
+        
+        System.out.println(sb);
+    }
+}
